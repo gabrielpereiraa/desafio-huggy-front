7864bbe4de4824af73eccf7d01ed4b8110172205
Gabriel da Silva Pereira <gabriel-xp288@hotmail.com>
2021-10-02 20:26:18 -0300

    Primeiro commit - init vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Desafio Huggy"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Roboto;
+  font-style: normal;
+  margin-top: 60px;
+}
+</style>
